92c7f35e463d33a81638a3ec3292c545b9cad653
Maksim Kazadaev <kazadaevms@yandex.ru>
2022-05-13 10:11:36 +0300

    Add RSI indicator

--- tester_trade_log/models/example_model.py
+++ tester_trade_log/models/example_model.py
@@ -14,42 +14,42 @@ import os.path
 
 
 def fill_momentum_indicators(df):
-    df['rsi'] = mp.RSIIndicator(df['close']).rsi()
-    df['tsi'] = mp.TSIIndicator(df['close']).tsi()
-    df['kama'] = mp.KAMAIndicator(df['close']).kama()
-    df['roc'] = mp.ROCIndicator(df['close']).roc()
-    df['srsi'] = mp.StochRSIIndicator(df['close']).stochrsi()
-    df['ppo'] = mp.PercentagePriceOscillator(df['close']).ppo()
-    df['pvo'] = mp.PercentageVolumeOscillator(df['close']).pvo()
+    df["rsi"] = mp.RSIIndicator(df["close"]).rsi()
+    df["tsi"] = mp.TSIIndicator(df["close"]).tsi()
+    df["kama"] = mp.KAMAIndicator(df["close"]).kama()
+    df["roc"] = mp.ROCIndicator(df["close"]).roc()
+    df["srsi"] = mp.StochRSIIndicator(df["close"]).stochrsi()
+    df["ppo"] = mp.PercentagePriceOscillator(df["close"]).ppo()
+    df["pvo"] = mp.PercentageVolumeOscillator(df["close"]).pvo()
 
 
 def fill_trend_indicators(df):
-    df['aroon'] = tr.AroonIndicator(df['close']).aroon_indicator()
-    df['macd'] = tr.MACD(df['close']).macd()
-    df['ema'] = tr.EMAIndicator(df['close']).ema_indicator()
-    df['sma'] = tr.SMAIndicator(df['close'], window=7).sma_indicator()
-    df['wma'] = tr.WMAIndicator(df['close']).wma()
-    df['trix'] = tr.TRIXIndicator(df['close']).trix()
-    df['ksti'] = tr.KSTIndicator(df['close']).kst()
-    df['dpoi'] = tr.DPOIndicator(df['close']).dpo()
-    df['stc'] = tr.STCIndicator(df['close']).stc()
+    df["aroon"] = tr.AroonIndicator(df["close"]).aroon_indicator()
+    df["macd"] = tr.MACD(df["close"]).macd()
+    df["ema"] = tr.EMAIndicator(df["close"]).ema_indicator()
+    df["sma"] = tr.SMAIndicator(df["close"], window=7).sma_indicator()
+    df["wma"] = tr.WMAIndicator(df["close"]).wma()
+    df["trix"] = tr.TRIXIndicator(df["close"]).trix()
+    df["ksti"] = tr.KSTIndicator(df["close"]).kst()
+    df["dpoi"] = tr.DPOIndicator(df["close"]).dpo()
+    df["stc"] = tr.STCIndicator(df["close"]).stc()
 
 
 def fill_volatility_indicators(df):
-    df['blb'] = vlt.BollingerBands(df['close']).bollinger_hband_indicator()
-    df['ulc'] = vlt.UlcerIndex(df['close']).ulcer_index()
+    df["blb"] = vlt.BollingerBands(df["close"]).bollinger_hband_indicator()
+    df["ulc"] = vlt.UlcerIndex(df["close"]).ulcer_index()
 
 
 def fill_volume_indicators(df):
-    df['obv'] = vlm.OnBalanceVolumeIndicator(df['close'], df['volume']).on_balance_volume()
-    df['frc'] = vlm.ForceIndexIndicator(df['close'], df['volume']).force_index()
-    df['vpt'] = vlm.VolumePriceTrendIndicator(df['close'], df['volume']).volume_price_trend()
-    df['npt'] = vlm.NegativeVolumeIndexIndicator(df['close'], df['volume']).negative_volume_index()
+    df["obv"] = vlm.OnBalanceVolumeIndicator(df["close"], df["volume"]).on_balance_volume()
+    df["frc"] = vlm.ForceIndexIndicator(df["close"], df["volume"]).force_index()
+    df["vpt"] = vlm.VolumePriceTrendIndicator(df["close"], df["volume"]).volume_price_trend()
+    df["npt"] = vlm.NegativeVolumeIndexIndicator(df["close"], df["volume"]).negative_volume_index()
 
 
 def create_old_indicators(df, shift):
     for column in df.columns:
-        new_column = 'new_' + str(shift) + '_' + column
+        new_column = "new_" + str(shift) + "_" + column
         df[new_column] = df[column].shift(shift)
 
 
@@ -69,10 +69,9 @@ class Model:
         pass
 
     def create_data(self, horizon: int, refill: bool = False, head: int = 20, tail: int = 10):
-        output = "analysis/data/indicator_data/" + self._ticker + '_' + str(self._period.total_seconds()) + '_' + str(
-            horizon) + '.csv'
+        output = "analysis/data/indicator_data/" + self._ticker + "_" + str(self._period.total_seconds()) + "_" + str(horizon) + ".csv"
         if os.path.exists(output) and refill is False:
-            self._df = pd.read_csv(output, sep='\t')
+            self._df = pd.read_csv(output, sep="\t")
             print("Already exist")
             return
         data_iterator = enumerate(DataIterator(self._data_directory, self._ticker, self._period))
@@ -83,7 +82,7 @@ class Model:
             for i, (time, price, high, low, volume) in enumerate(intraday_iterator):
                 intraday_history.append([price, volume])
                 intraday_price.append(price)
-            df = pd.DataFrame(intraday_history, columns=['close', 'volume'])
+            df = pd.DataFrame(intraday_history, columns=["close", "volume"])
             fill_momentum_indicators(df)
             fill_trend_indicators(df)
             fill_volatility_indicators(df)
@@ -93,36 +92,35 @@ class Model:
                 create_old_indicators(df, hor)
             for k in range(horizon):
                 intraday_price.append(0)
-            df['new_price'] = intraday_price[horizon:]
-            df['day'] = day_index
+            df["new_price"] = intraday_price[horizon:]
+            df["day"] = day_index
             df = df.dropna()
-            df = df.iloc[head:-(horizon + tail)]
+            df = df.iloc[head : -(horizon + tail)]
             lst_of_df.append(df)
         all_data = pd.concat(lst_of_df, ignore_index=True)
-        print('Creation completed successfully')
+        print("Creation completed successfully")
         self._df = all_data
-        all_data.to_csv(output, sep='\t')
+        all_data.to_csv(output, sep="\t")
 
     def fit(self, test_size: int = 0.2):
-        self._count_of_days = max(self._df['day'])
+        self._count_of_days = max(self._df["day"])
         self._test_size = test_size
         # todo self._train_data = df[where df[days] < (1 - test_size) * count_of_days]
         print(self._count_of_days)
-        self._train_data = self._df[self._df['day'] < (1 - self._test_size) * self._count_of_days].drop(['day'], axis=1)
+        self._train_data = self._df[self._df["day"] < (1 - self._test_size) * self._count_of_days].drop(["day"], axis=1)
         # self._tree.fit(self._train_data.drop(['new_price'], axis=1), self._train_data['new_price'])
-        self._catboost.fit(self._train_data.drop(['new_price'], axis=1), self._train_data['new_price'])
+        self._catboost.fit(self._train_data.drop(["new_price"], axis=1), self._train_data["new_price"])
         print("ALL", self._train_data.shape)
 
     def predict(self):
-        self._test_data = self._df[self._df['day'] > (1 - self._test_size) * self._count_of_days].drop(['day'], axis=1)
+        self._test_data = self._df[self._df["day"] > (1 - self._test_size) * self._count_of_days].drop(["day"], axis=1)
         # ans = self._tree.predict_proba(self._test_data.drop(['new_price'], axis=1))
-        return self._catboost.score(X=self._test_data.drop(['new_price'], axis=1), y=self._test_data['new_price'])
+        return self._catboost.score(X=self._test_data.drop(["new_price"], axis=1), y=self._test_data["new_price"])
         # return self._tree.score(self._test_data.drop(['new_price'], axis=1), self._test_data['new_price'])
 
 
 # p = Model("analysis/data/tickers_trade_log", "SBER", datetime.timedelta(minutes=1), max_depth=6)
-p = Model("analysis/data/tickers_trade_log", "SBER", datetime.timedelta(minutes=1), iterations=2, learning_rate=0.7,
-          depth=2)
+p = Model("analysis/data/tickers_trade_log", "SBER", datetime.timedelta(minutes=1), iterations=2, learning_rate=0.7, depth=2)
 lst = []
 for i in range(1, 4):
     p.create_data(i)
--- tester_trade_log/strategies/indicators.py
+++ tester_trade_log/strategies/indicators.py
@@ -2,6 +2,52 @@ from tester_trade_log.tester import Indicator, Tester
 from collections import deque
 
 
+class RSI(Indicator):
+    def __init__(self, length):
+        self._length = length
+        self._changes_queue = deque()
+        self._total_sum = 0
+        self._positive_sum = 0
+        self._last_price = None
+        self._rsi_history = []
+
+    def get_init_periods(self) -> int:
+        return self._length
+
+    def initialize(self, t: "Tester"):
+        prices = t.get_today_price_history()
+        assert len(prices) == self._length
+        self._last_price = prices[-1]
+        changes = np.diff(prices)
+        self._changes_queue = deque(map(float, changes))
+        self._total_sum = float(np.abs(changes).sum())
+        self._positive_sum = float(changes[changes > 0].sum())
+        self._rsi_history.append(self.get_current_value())
+
+    def on_tick(self, t: "Tester"):
+        new_price = t.get_current_price()
+        new_change = new_price - self._last_price
+        self._last_price = new_price
+        old_change = self._changes_queue.popleft()
+        self._changes_queue.append(new_change)
+        self._total_sum += abs(new_change) - abs(old_change)
+        if new_change > 0:
+            self._positive_sum += new_change
+        if old_change > 0:
+            self._positive_sum -= old_change
+        self._rsi_history.append(self.get_current_value())
+
+    def get_current_value(self):
+        return self._positive_sum / self._total_sum * 100 if self._total_sum else 50
+
+    def get_values_history(self):
+        return self._rsi_history
+
+    def clear(self, t: "Tester"):
+        self._rsi_history.clear()
+        self._last_price = None
+
+
 class SMA(Indicator):
     def __init__(self, tact: int, len_of_window: int, mode: str = "simple", wait: bool = True):
         """
